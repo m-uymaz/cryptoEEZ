1a9ef7142779155531b6c901b1636b84f9b8fa1c
Mihail Uymaz <michael.m.uymaz@gmail.com>
2021-11-02 18:44:05 +0200

    second try

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 5
+        maxAge: Date.now() + 3600000 * 24 * 7
     },
     store: MongoStore.create({
         mongoUrl: dbUrl,
